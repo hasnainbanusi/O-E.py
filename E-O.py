924c4b35a56f7d6c9f5523b5ed3dc03b94a20ed2
Hasnain Banusi <your-github-hasnain10.official@gmail.com>
2026-05-08 15:59:31 +0500

    comit

---
+++ E-O.py
@@ -0,0 +1,8 @@
+#This is code check if the number is EVEN or ODD 
+
+num = int(input("Enter the number : "))
+
+if num % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
